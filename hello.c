d789523e34ac297299ca81e7ab82a262d2acd53d
Kelsonz <minghengww@outlook.com>
2022-05-29 16:57:30 +0800

    我现在要amend一下commit，我现在要reword一下commit，刚才的效果不是我想要的，我应该在cfile里面增加新的注释，然后切回去再进行注释，最后在合并起来

--- hello.c
+++ hello.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+<<<<<<< HEAD
 /* main函数，打印你好世界  ***************************************************/
+=======
+/**
+ * @brief      main
+ *
+ * @details    
+ *
+ * @param      argc
+ *
+ * @return     int 
+ */
+>>>>>>> fda59e1 (我现在要amend一下commit，我现在要reword一下commit，刚才的效果不是我想要的，我应该在cfile里面增加新的注释，然后切回去再进行注释，最后在合并起来)
 int main(int argc, char *argv[])
 {
   printf("Hello,\n");
