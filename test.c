d7849a56ed820eaa5602a1206392cff6e936e961
Kelsonz <minghengww@outlook.com>
2022-05-31 12:23:27 +0800

    现在我知道了，打开 pangu-spacing-mode 并不会直接保存到文件中。但是再进行修改了就会加入其中了

--- test.c
+++ test.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+/* 现在看看，不开pangu-spacingmode，然后开了以后保存，是不是就固定下来了？ */
+/* 看看 pangu-spacing 是否正常工作？ */
+/* 学习 Org-mode 的排序系统。 */
 /* 现在再添加一条注释，好像就可以了 */
 int main(int argc, char *argv[])
 {
