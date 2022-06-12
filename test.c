23c9235210743cbd8c05a1f13dcb810bedd21bc7
Kelsonz <minghengww@outlook.com>
2022-06-12 10:49:01 +0800

    修改注释

--- test.c
+++ test.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 
-/* 现在看看，不开 pangu-spacingmode，然后开了以后保存，是不是就固定下来了？ */
-/* 看看 pangu-spacing 是否正常工作？ */
-/* 学习 Org-mode 的排序系统。 */
-/* 现在再添加一条注释，好像就可以了 */
-/* 我真的是服了，rainbow-mode 咋弄的？ ***************************************/
+/* 你好世界 */
 int main(int argc, char *argv[])
 {
   printf("Hello, World!\n");
